413d13db5d8b39a58018987b5dbe4e5867d378e3
fermayo <fermayo@gmail.com>
2015-02-23 19:59:05 +0100

    fix weave password handling

--- run.sh
+++ run.sh
@@ -15,11 +15,12 @@ if [ "${WEAVE_LAUNCH}" = "**None**" ]; then
 else
     echo "=> Resetting weave on the node"
     /weave reset
-    echo "=> Running: weave launch \"${WEAVE_LAUNCH}\""
-    if [ -z "${WEAVE_PASSWORD}" ]; then
-        echo "!! WARNING: No \$WEAVE_PASSWORD set!"
+    if [ ! -z "${WEAVE_PASSWORD}" ]; then
+        echo "=> Running: weave launch -password XXXXXX ${WEAVE_LAUNCH}"
         /weave launch -password ${WEAVE_PASSWORD} ${WEAVE_LAUNCH} || true
     else
+        echo "!! WARNING: No \$WEAVE_PASSWORD set!"
+        echo "=> Running: weave launch ${WEAVE_LAUNCH}"
         /weave launch ${WEAVE_LAUNCH} || true
     fi
     sleep 2
