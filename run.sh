a38c90f93cd3c622d607814d50deca689c9804c0
HECKEL Maxime <heckelmaxime@gmail.com>
2015-11-19 12:49:42 +0100

    add no-discovery flag to weave run command

--- run.sh
+++ run.sh
@@ -27,7 +27,7 @@ else
     if [ ! -z "${WEAVE_PASSWORD}" ]; then
         echo "=> Running: weave launch -password XXXXXX ${WEAVE_LAUNCH}"
         echo "=> Peer count: ${TUTUM_PEER_COUNT}"
-        /weave --local launch-router --ipalloc-range=10.128.0.0/10 --no-dns --init-peer-count ${TUTUM_PEER_COUNT} --password ${WEAVE_PASSWORD} ${WEAVE_LAUNCH} || true
+        /weave --local launch-router --ipalloc-range=10.128.0.0/10 --no-dns --no-discovery --init-peer-count ${TUTUM_PEER_COUNT} --password ${WEAVE_PASSWORD} ${WEAVE_LAUNCH} || true
     else
         echo "!! WARNING: No \$WEAVE_PASSWORD set!"
         echo "=> Running: weave launch ${WEAVE_LAUNCH}"
