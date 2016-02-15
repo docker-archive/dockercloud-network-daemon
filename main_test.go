5529402df87cd676ebec00e0d58c25bfb099e3ee
Maxime Heckel <heckelmaxime@gmail.com>
2016-02-15 19:02:48 +0100

    update test suit and integration test

--- main_test.go
+++ main_test.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"log"
-	"testing"
-)
+import "testing"
 
 func Test_nodeEventHandler(t *testing.T) {
 	eventType1 := "node"
@@ -13,27 +10,23 @@ func Test_nodeEventHandler(t *testing.T) {
 	state3 := "Terminated"
 	action1 := "create"
 	action2 := "update"
-	Msg := "Failed API call: 404 NOT FOUND"
+	Msg := "Couldn't find any DockerCloud credentials in ~/.docker/config.json or environment variables DOCKERCLOUD_USER and DOCKERCLOUD_APIKEY"
 
 	err := nodeEventHandler(eventType1, state1, action2)
 	err2 := nodeEventHandler(eventType2, state1, action2)
+	err3 := nodeEventHandler(eventType2, state3, action2)
+	err4 := nodeEventHandler(eventType1, state2, action2)
+	err5 := nodeEventHandler(eventType1, state3, action1)
+
 	if err2 != nil {
-		log.Println(err)
 		t.Error("Expected empty error message, got ", err2.Error())
 	}
-
-	err3 := nodeEventHandler(eventType2, state3, action2)
 	if err3 != nil {
 		t.Error("Expected empty error message, got ", err3.Error())
 	}
-
-	err4 := nodeEventHandler(eventType1, state2, action2)
 	if err4 != nil {
 		t.Error("Expected empty error message, got ", err4.Error())
 	}
-
-	err5 := nodeEventHandler(eventType1, state3, action1)
-
 	if err != nil && err5 != nil {
 		if err.Error() != Msg || err5.Error() != Msg {
 			t.Error("Expected error, got ", err.Error())
